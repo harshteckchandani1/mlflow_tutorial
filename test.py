bd0256e7db26f00feb168ced9cde03386a159d66
harshteckchandani1 <lt.aids1@gsv.ac.in>
2025-09-22 13:21:02 +0530

    till 1.01

---
+++ test.py
@@ -0,0 +1,15 @@
+import mlflow
+
+print("Printing URI scheme below: ")
+
+print(mlflow.get_tracking_uri())
+
+print("\n")
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+print("printing new tracking uri ")
+
+print(mlflow.get_tracking_uri())
+
+print("\n")
