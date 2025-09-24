890278212c350c404236860887e372fb38a5a904
harshteckchandani1 <lt.aids1@gsv.ac.in>
2025-09-24 16:12:13 +0530

    addtion of different test experiment name for user

--- train_dataset.py
+++ train_dataset.py
@@ -33,8 +33,8 @@ x_train, x_test, y_train, y_test  = train_test_split(x, y, test_size=0.10, rando
 max_depth = 5
 n_estimators = 8 
 
-# Mention experiment below
-# mlflow.set_experiment("name of experiment")
+# Mention experiment name below for user convenience
+mlflow.set_experiment("test123")
 
 with mlflow.start_run():
     rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators, random_state=42)
